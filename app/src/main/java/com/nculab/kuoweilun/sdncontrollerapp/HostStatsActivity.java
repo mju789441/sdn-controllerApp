d11a0bc9b359843b2a6b1d1cd55e939198d6a63a
kuo <mju789441@gmail.com>
2018-04-14 01:32:34 +0800

    hoststat更改詳細資料顯示方式

--- app/src/main/java/com/nculab/kuoweilun/sdncontrollerapp/HostStatsActivity.java
+++ app/src/main/java/com/nculab/kuoweilun/sdncontrollerapp/HostStatsActivity.java
@@ -1,10 +1,17 @@
 package com.nculab.kuoweilun.sdncontrollerapp;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +20,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.sql.Array;
+import java.util.ArrayList;
 
 /**
  * Created by Kuo Wei Lun on 2017/12/3.
@@ -24,7 +33,7 @@ public class HostStatsActivity extends AppCompatActivity {
     private View activityView;
     private String connect_IP;
     private String switch_ID;
-    private TextView textView_content;
+    private TableLayout tableLayout;
     private Button button_backToHost;
     private Host host;
     private String hostCotent;
@@ -64,10 +73,8 @@ public class HostStatsActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        textView_content = (TextView) findViewById(R.id.textView_content);
+        tableLayout = (TableLayout) findViewById(R.id.tableLayout);
         button_backToHost = (Button) findViewById(R.id.button_backToHost);
-        hostCotent = "ID: " + host.ID + "\nport: " + host.port + "\nmac: " + host.mac +
-                "\nspeed: " + host.speed + "\n";
     }
 
     private void setListeners() {
@@ -84,7 +91,7 @@ public class HostStatsActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    if (!host.port.equals( "LOCAL")) {
+                    if (!host.port.equals("LOCAL")) {
                         JSONObject hostStats = controllerURLConnection
                                 .getPortStats(switch_ID + "/" + host.port)
                                 .getJSONArray(switch_ID).getJSONObject(0);
@@ -100,23 +107,70 @@ public class HostStatsActivity extends AppCompatActivity {
     }
 
     private void setStats(final JSONObject hostStats) {
-        String temp = "";
+        final Context context = this;
+        final ArrayList<String> name = new ArrayList<String>();
+        final ArrayList<String> text = new ArrayList<String>();
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    name.add("ID");
+                    text.add(host.ID);
+                    break;
+                case 1:
+                    name.add("port");
+                    text.add(host.port);
+                    break;
+                case 2:
+                    name.add("mac");
+                    text.add(host.mac);
+                    break;
+                case 3:
+                    name.add("speed");
+                    text.add(host.speed);
+                    break;
+            }
+        }
         JSONArray names = hostStats.names();
         for (int i = 0; i < names.length(); i++) {
             try {
-                temp += names.getString(i) + ": " + hostStats.getString(names.getString(i)) + "\n";
+                name.add(names.getString(i));
+                text.add(hostStats.getString(names.getString(i)));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+
         }
-        final String stats = temp;
         handler.post(new Runnable() {
-                         @Override
-                         public void run() {
-                             textView_content.setText(hostCotent + stats);
-                         }
-                     }
-        );
+            @Override
+            public void run() {
+                for (int i = 0; i < name.size(); i++) {
+                    //component
+                    TableRow tableRow = new TableRow(context);
+                    TextView textView = new TextView(context);
+                    TextView textView1 = new TextView(context);
+                    View view = new View(context);
+                    //setting
+                    view.setBackgroundColor(Color.GRAY);
+                    view.setMinimumHeight(2);
+                    //margin
+                    TableRow.LayoutParams layoutParams = new TableRow.LayoutParams();
+                    layoutParams.setMargins(50, 0, 50, 0);
+                    textView.setLayoutParams(layoutParams);
+                    textView1.setLayoutParams(layoutParams);
+                    textView.setTextSize(20);
+                    textView.setText(name.get(i) + ": ");
+                    textView1.setTextSize(20);
+                    textView1.setGravity(Gravity.RIGHT);
+                    textView1.setText(text.get(i));
+                    //addView
+                    if (i != 0)
+                        tableLayout.addView(view);
+                    tableRow.addView(textView);
+                    tableRow.addView(textView1);
+                    tableLayout.addView(tableRow);
+                }
+            }
+        });
     }
 
 }
